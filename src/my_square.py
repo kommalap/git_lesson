679310ef42e8e76764c3e3d4d61aac800f81b0e1
rk1027332 <kommalap@yahoo.com>
2016-12-17 13:31:35 -0500

    x -> y and square 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,6 +1,6 @@
-def my_square(x):
+def my_square(y):
   """ take a value and return its squared value
       uses the ** operator to compute squares
   """
-  return(x ** 2)
-print my_square(4)
+  return(y ** 2)
+print my_square(42)
